e1791b92c744e5a7a91fa47769dc82d884e5d5dd
sergvazq99 <125486467+sergvazq99@users.noreply.github.com>
2025-11-28 00:09:19 +0100

    Add files via upload

--- PracticaAW25-26/recurso-bootstrap/routes/vehiculo.js
+++ PracticaAW25-26/recurso-bootstrap/routes/vehiculo.js
@@ -1,40 +1,69 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const path = require('path');
+const db = require("../db");
 
-const { vehiculos } = require("../data");
+// Configuración de Multer
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, path.join(__dirname, '../public/recurso-imagenes/vehiculos')); // carpeta donde se guardan las imágenes
+  },
+  filename: function(req, file, cb) {
+    // Evitar conflictos de nombres: fecha + nombre original
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    const ext = path.extname(file.originalname);
+    cb(null, file.fieldname + '-' + uniqueSuffix + ext);
+  }
+});
 
-router.get("/",(req,res)=>{
+const upload = multer({ storage: storage });
+
+router.get("/",async (req,res)=>{ 
   res.render("vehicle");
 });
 
-router.get("/",(req,res)=>{
-  res.render("vehicles",{v:vehiculos,usuario:req.session.usuario});
+router.post("/",upload.single("image"), async (req,res)=>{
+  const {matricula,marca,modelo,autonomia,anyo_matriculacion,plazas,color,concesionario}=req.body;
+
+  
+  const estadoVehiculo="disponible";
+  const imagen = req.file ? req.file.filename : null;
+  try{
+    const [c] = await db.query("SELECT id FROM concesionarios WHERE nombre = ?",[concesionario]);
+            
+    if (c.length === 0) {
+          return res.status(400).json({
+                ok: false,
+                error: "El concesionario no existe"
+          });
+    }
+    const idConcesionario=c[0].id;
+    const [result]=await db.query("INSERT INTO vehiculos (matricula,marca,modelo,autonomia,anyo_matriculacion,plazas,estadoVehiculo,imagen,color,idConcesionario)"+ 
+      "VALUES (?, ?, ?, ?, ?, ?, ?, ?,?,?)",[matricula,marca,modelo,autonomia,anyo_matriculacion,plazas,estadoVehiculo,imagen,color,idConcesionario]);
+
+      res.json({ ok: true, id: result.insertId });
+  }
+  catch (err) {
+      console.error(err);
+      res.status(500).json({ ok: false, error: "Error al insertar vehículo" });
+  }
+  
 });
 
-router.post("/",(req,res)=>{
-  const {matricula,marca,modelo,anyo_matriculacion,plazas,autonomia,color,concesionario}=req.body;
-  let idVehiculo;
-
-    if(vehiculos.length>0){
-      idVehiculo=vehiculos[vehiculos.length-1].id+1;
-    }
-    else{
-      idVehiculo=1;
-    }
-  const estado="disponible";
-  const vehiculo={id:idVehiculo,matricula,marca,modelo,anyo_matriculacion,plazas,autonomia,color,concesionario,estadoVehiculo:estado};
-  vehiculos.push(vehiculo);
-
-  if(!req.session.vehiculos){
-    req.session.vehiculos=[];
+router.get("/vehiculos",async (req,res)=>{
+  const idCon = req.session.usuario.idConcesionario;
+  try{
+    const [vehiculos]=await db.query("SELECT vehiculos.*,concesionarios.nombre AS concesionario FROM vehiculos "+
+      "INNER JOIN concesionarios ON vehiculos.idConcesionario = concesionarios.id WHERE vehiculos.idConcesionario = ?",[idCon]);
+    res.render("vehicles",{v:vehiculos,usuario:req.session.usuario});
+  }
+  catch (err) {
+    console.error(err);
+    res.status(500).send("Error al cargar vehículos");
   }
 
-  req.session.vehiculos.push(vehiculo);
-  res.render("vehicles",{v:req.session.vehiculos,usuario:req.session.usuario});
-});
-
-router.get("/vehiculos",(req,res)=>{
-  res.render("vehicles",{v:req.session.vehiculos,usuario:req.session.usuario});
+  
 });
 
 module.exports = router;
